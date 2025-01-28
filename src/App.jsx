ba0c3282a77827ae98a28b09b12e46b6141ea9f6
pro_dip <prodip.sarker@sslwireless.com>
2025-01-28 16:41:15 +0600

    added routers, role based manu, api call and module based structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,38 @@
-import './App.css'
-// import Login from './layout/Login'
-import MainLayout from './layout/MainLayout'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Suspense } from "react";
+import routes from "./routes";
+import MainLayout from "./layout/MainLayout";
+import ErrorBoundary from "./pages/ErrorBoundary";
+import Login from "./pages/Login";
+import PrivateRoute from "./auth/PrivateRoute";
+import { AuthProvider } from "./auth/AuthContext";
+import { Spin } from "antd";
 
-function App() {
+const App = () => (
+  <ErrorBoundary>
+    <Router>
+      <Suspense fallback={<Spin size="large"></Spin>}>
+        <AuthProvider>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            {routes.map(({ path, component: Component }, index) => (
+              <Route
+                key={index}
+                path={path}
+                element={
+                  <PrivateRoute>
+                    <MainLayout>
+                      <Component />
+                    </MainLayout>
+                  </PrivateRoute>
+                }
+              />
+            ))}
+          </Routes>
+        </AuthProvider>
+      </Suspense>
+    </Router>
+  </ErrorBoundary>
+);
 
-  return (
-    <>
-      {/* <Login/> */}
-      <MainLayout/>
-    </>
-  )
-}
-
-export default App
+export default App;
